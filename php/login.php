fa2cc0dec8c2707d24c1de68dd19edb2342c04a8
Niniblbl <anitapesselc@gmail.com>
2025-06-03 17:00:49 +0200

    init

---
+++ php/login.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_USER', 'cir2_projet');
+  define('DB_PASSWORD', 'cir2_mdp_projet');
+  define('DB_NAME', 'comweb_project');
+  define('DB_SERVER', 'localhost');
+  define('DB_PORT', '3306');
+?>
